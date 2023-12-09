e49efe202568d77508af1f4b58e88c36261ea077
mashuir1610 <mashuirrahamanshuvo@gmail.com>
2023-12-09 21:29:59 +0600

    first commit

---
+++ app/Mail/SendEmailQueueDemo.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class SendEmailQueueDemo extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->subject('How To Send E-mail Using Queue In laravel 10')
+            ->view('email.demo');
+    }
+}
---
+++ resources/views/email/demo.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>How To Send Mail Using Queue In Laravel 10 - shouts.dev</title>
+</head>
+<body>
+
+<center>
+    <h2>
+        <a href="https://shouts.dev/">Visit Our Website : shouts.dev</a>
+    </h2>
+</center>
+
+<p>Hello,</p>
+
+<p>This is a test mail. This mail send using queue listen in laravel 10.</p>
+
+<strong>Thanks & Regards.</strong>
+
+</body>
+</html>
